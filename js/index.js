78ac11eea53d4cf7a6b5d2824fad06af40d04809
Vania <vaniafatima@msn.com>
2023-09-28 17:16:20 -0300

    atualizando alguns campos

--- js/index.js
+++
@@ -1,7 +0,0 @@
-function Enviar() {
-    var nome = document.getElementById("name");
-
-    if (nome.value != "") {
-        alert('Obrigada sr(a) ' + nome.value + ' seus dados foram cadastrados com sucesso, até mais!!');
-    }
-}
